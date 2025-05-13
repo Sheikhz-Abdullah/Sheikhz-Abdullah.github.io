c8af8031456721e80ab756be6a876b21075c61b1
Abdullah Mubashir <abdullahmubashir009@gmail.com>
2025-05-13 20:55:18 +0400

    cursor added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
   document.querySelector("nav").classList.toggle("open");
 }
+
+const follower = document.getElementById('cursor-follower');
+    const offsetX = 30; // pixels to the right
+    const offsetY = 30; // pixels down
+
+    document.addEventListener('mousemove', (e) => {
+      follower.style.left = `${e.clientX + offsetX}px`;
+      follower.style.top = `${e.clientY + offsetY}px`;
+    });
